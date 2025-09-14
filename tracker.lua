c1aca0c837e53ba3250bce792594e4e8b94d4352
DontEatIvy <Yfknappe@gmail.com>
2025-09-14 22:57:41 +0200

    Fixed parsing bug in the html and...
    renamed the mai.lua so it is more compatabile with other lua scripts.

--- tracker.lua
+++ tracker.lua
@@ -1,12 +1,15 @@
 --location
+
 log("### recording flight data ###")
+
+
+script.logUi = false
 
 log("#f_log lat lon")
 
-local lastRecord = 0
 return function()
 
 	--if not lastRecord == controls.Time then
-		log( "#f_log " .. location.latitude .. " " .. location.longitude)
+	log( "#f_log " .. location.latitude .. " " .. location.longitude)
 	--#end
 end
