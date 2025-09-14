fb46088457a3cc69cb0832501a3f807bd3cecefb
DontEatIvy <Yfknappe@gmail.com>
2025-09-14 23:54:21 +0200

    Improved acuracy of the path and...
    made the lua script update less for smaler files.

--- tracker.lua
+++ tracker.lua
@@ -1,15 +1,21 @@
 --location
 
+local scipTicks = 10; -- amount of ticks skiped between entries
+
+
+
 log("### recording flight data ###")
 
-
+local timer = 0;
 script.logUi = false
 
 log("#f_log lat lon")
-
 return function()
 
-	--if not lastRecord == controls.Time then
-	log( "#f_log " .. location.latitude .. " " .. location.longitude)
-	--#end
+	if timer > scipTicks then
+		log( "#f_log " .. location.latitude .. " " .. location.longitude)
+		timer = 0
+	else
+		timer = timer + 1
+	end
 end
